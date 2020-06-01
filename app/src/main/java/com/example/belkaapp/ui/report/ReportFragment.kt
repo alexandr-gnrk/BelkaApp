06e0b7e52e23daa2295bff9bdc90b3f93f026e93
Alexandr Honcharuk <alexandr.gnrk@gmail.com>
2020-06-01 03:09:49 +0300

    Create template app

---
+++ app/src/main/java/com/example/belkaapp/ui/report/ReportFragment.kt
@@ -0,0 +1,30 @@
+package com.example.belkaapp.ui.report
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.belkaapp.R
+
+class ReportFragment : Fragment() {
+    private lateinit var reportViewModel: ReportViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        reportViewModel =
+            ViewModelProvider(this).get(ReportViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_slideshow)
+//        reportViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/belkaapp/ui/report/ReportViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.belkaapp.ui.report
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ReportViewModel: ViewModel()  {
+}
